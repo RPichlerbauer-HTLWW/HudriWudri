81b1d1009a9ec087345db43a3716c11785ad08c0
Raphael.P <77831872+RPichlerbauer-HTLWW@users.noreply.github.com>
2022-10-06 12:41:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
-workbox.routing.registerRoute(new RegExp('/.*'), new workbox.strategies.NetworkFirst());
+workbox.routing.registerRoute(new RegExp('/.*'), new workbox.strategies.StaleWhileRevalidate());
