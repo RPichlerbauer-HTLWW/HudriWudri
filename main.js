94fb687fb7e42411dc3dce1d3525d630b3b994ea
Raphael.P <77831872+RPichlerbauer-HTLWW@users.noreply.github.com>
2022-10-06 13:15:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ if ('serviceWorker' in navigator) {
     .register('./sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  document.querySelector(".hide").classList.toggle('hide', false);
+
+  deferredPrompt = e;
+});
+
+document.querySelector('.hide').addEventListener('click', async () => {
+  if (deferredPrompt !== null) {
+    deferredPrompt.prompt();
+
+    const { outcome } = await deferredPrompt.userChoice;
+
+    if (outcome === 'accepted') {
+      deferredPrompt = null;
+    }
+  }
+});
